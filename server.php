9a41c942476d7cd2e4d39106354cdefa9c41e504
davidefois96 <davidefois360@gmail.com>
2024-04-18 18:06:32 +0200

    [add] new code

--- server.php
+++ server.php
@@ -2,9 +2,11 @@
 
 $json_string = file_get_contents('dischi.json');
 
+var_dump($json_string);
+
 $list= json_decode($json_string);
 
-if (isset($_POST['newdiskTitle'])) {
+if (isset($_POST['newDiskTitle'])) {
  $new_disk=[
 
   'title' => $_POST['newDiskTitle'],
@@ -14,7 +16,8 @@ if (isset($_POST['newdiskTitle'])) {
   'genre' => $_POST['newDiskGenre']
 
  ];
- $list[]= $new_disk;
+ $list[] = $new_disk;
+
  file_put_contents('dischi.json', json_encode($list));
 
 }
